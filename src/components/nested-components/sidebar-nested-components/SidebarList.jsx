3794f3aac28efbb9ca4fb195be437570c630ff2a
Anurag <anuragsayshelloworld@gmail.com>
2025-09-02 21:59:21 +0545

    Unimportant: Redoing previous change that I lost

--- src/components/nested-components/sidebar-nested-components/SidebarList.jsx
+++ src/components/nested-components/sidebar-nested-components/SidebarList.jsx
@@ -1,16 +1,17 @@
 import { MoreVertical } from "lucide-react";
 import React, { useEffect, useState } from "react";
 import { fetchDocumentsByAuthor } from "../../../firebase";
+import { useNavigate } from "react-router-dom";
 
 function SidebarList({ expanded }) {
   const auth = JSON.parse(localStorage.getItem("auth"));
   const [items, setItems] = useState([]);
-
+  const navigate = useNavigate();
   useEffect(() => {
     const getDocsByAuthor = async () => {
       if (!auth?.username) return;
       const response = await fetchDocumentsByAuthor(auth.username);
-      setItems(response); // response is already an array
+      setItems(response);
     };
     getDocsByAuthor();
   }, [auth?.username]);
@@ -27,6 +28,7 @@ function SidebarList({ expanded }) {
       <div className="space-y-1 px-2 pb-2 pr-1 overflow-y-auto flex-1 scrollbar-hide">
         {items.map((doc) => (
           <div
+            onClick={() => navigate(`/viewer/${doc.id}`, { state: doc })}
             key={doc.id}
             className="group flex justify-between items-center px-3 py-2 rounded-md hover:bg-gray-200 cursor-pointer text-sm text-gray-800"
           >
